cf0fb10fa966af59bc09187741052730b338466a
minis <minis>
2023-08-06 10:18:05 +0800

    oo

--- datapreprocessing.py
+++ datapreprocessing.py
@@ -1,5 +1,6 @@
 import json;
 from cnredis import r
+import math;
 def getonelistfromredis(s_code):
     key = 'factor_'+s_code
     oneindexlist = r.lrange(key,0,-1);
@@ -7,18 +8,27 @@ def getonelistfromredis(s_code):
     for one in oneindexlist:
             one=str2list(one);
             if len(one)==15:
+                toindex=list(range(2,len(one)));
+                one=digitization(one,toindex)
                 onelist.append(one);
     return onelist;
 
 
 def str2list(strlist):
     return json.loads(strlist);
+def nonum():
+    return round(-1*math.pi,6);
+def isnonum(num):
+    if num==nonum():
+        return True;
+    else:
+        return False;
 
 
 def digitization(li,toindex):
     for index in toindex:
         if li[index]=='':
-            li[index]=-1;
+            li[index]=nonum();
         else:
             li[index]=float(li[index])
     return li;
--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 from cnredis import r
 from datapreprocessing import digitization
 from datapreprocessing import getonelistfromredis
-import json
+from datapreprocessing import nonum;
+from datapreprocessing import isnonum;
+print(nonum())
+print(isnonum(nonum()))
 li=getonelistfromredis('002911');
 for i in li:
-    toindex=list(range(2,len(i)));
-    digitization(i,toindex);
     print(i);
 
